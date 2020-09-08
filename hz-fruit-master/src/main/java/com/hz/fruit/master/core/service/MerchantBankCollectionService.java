454f1fdb7b661706d7c19cc766f3035e5f853e94
df-HelloWorld <duanfeng_1712@qq.com>
2020-09-08 17:33:56 +0800

    edit_4

---
+++ hz-fruit-master/src/main/java/com/hz/fruit/master/core/service/MerchantBankCollectionService.java
@@ -0,0 +1,13 @@
+package com.hz.fruit.master.core.service;
+
+
+import com.hz.fruit.master.core.common.service.BaseService;
+
+/**
+ * @Description 银行收款信息的Service层
+ * @Author yoko
+ * @Date 2020/5/21 17:01
+ * @Version 1.0
+ */
+public interface MerchantBankCollectionService<T> extends BaseService<T> {
+}
