63d4aa87a9aec6955002f2043bf05f604b623816
df-HelloWorld <duanfeng_1712@qq.com>
2020-09-11 10:41:29 +0800

    edit_10

---
+++ hz-fruit-master/src/main/java/com/hz/fruit/master/core/service/MobileCardService.java
@@ -0,0 +1,13 @@
+package com.hz.fruit.master.core.service;
+
+
+import com.hz.fruit.master.core.common.service.BaseService;
+
+/**
+ * @Description 手机卡的Service层
+ * @Author yoko
+ * @Date 2020/5/18 17:22
+ * @Version 1.0
+ */
+public interface MobileCardService<T> extends BaseService<T> {
+}
